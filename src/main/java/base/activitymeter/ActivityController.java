7bd60b95a9947f14ea22b3d8bacee7e01f4c23bb
Mosby <laur@hm.edu>
2017-11-28 12:15:13 +0100

    filter button oeffnen geht nicht

--- src/main/java/base/activitymeter/ActivityController.java
+++ src/main/java/base/activitymeter/ActivityController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/activity")
@@ -11,8 +12,8 @@ public class ActivityController {
   
   @Autowired
   private ActivityRepository activityRepository;
-  
-  
+
+
   @GetMapping
   public ArrayList<Activity> listAll() {
       ArrayList<Activity> activities = new ArrayList<>();
@@ -20,10 +21,22 @@ public class ActivityController {
       return activities;
   }
 
-  @GetMapping("{id}")
-  public Activity find(@PathVariable Long id) {
-      return activityRepository.findOne(id);
+  //@RequestMapping(value ="{tags}", method = RequestMethod.GET)
+    @GetMapping("{tags}")
+  public ArrayList<Activity> listAllFilter(@PathVariable String tags) {
+      ArrayList<Activity> activities = new ArrayList<>();
+      ArrayList<Activity> activitiesFiltered = new ArrayList<>();
+      activityRepository.findAll().forEach(activity -> activities.add(activity));
+      activities.stream().filter(x->x.getTags().
+              equals(tags)).forEach(activity -> activitiesFiltered.add(activity));
+      return activitiesFiltered;
+
+
   }
+//  @GetMapping("{id}")
+//  public Activity find(@PathVariable Long id) {
+//      return activityRepository.findOne(id);
+//  }
 
   @PostMapping
   public Activity create(@RequestBody Activity input) {
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,126 @@
+var app = angular.module('ActivityMeterApp', ['ui.bootstrap']);
+
+//app.config(function ($qProvider) {
+//    $qProvider.errorOnUnhandledRejections(false);
+//});
+
+function loadActivities ($scope, $http){
+     		$http({
+            		 method : 'GET',
+            		 /*
+            		 url: (window.location.hostname === 'localhost' ?
+          				'http://localhost:8080/activity' :
+                         'https://activityexample.herokuapp.com/activity')
+                  */
+                  url: 'activity'
+
+         		}).then(function (response) {
+          			 $scope.activities = response.data;
+       		});
+       	}
+
+
+app.controller('ActivityCtrl', function ($scope, $http, $dialog) {
+
+  	loadActivities($scope, $http);
+
+
+  	var addDialogOptions = {
+    	controller: 'AddActivityCtrl',
+    	templateUrl: './activityAdd.html'
+  	};
+
+  	$scope.add = function(activity){
+    	$dialog.dialog(angular.extend(addDialogOptions, {})).open().then(function (){
+    	    loadActivities($scope, $http);
+        }) ;
+  	};
+
+
+  	//-------------------
+  	var filterDialogOptions = {
+    	controller: 'FilterActivityCtrl',
+    	templateUrl: './activityFilter.html'
+  	};
+
+  	$scope.filter = function(activity){
+    	$dialog.dialog(angular.extend(filterDialogOptions, {})).open().then(function (){
+    	    loadActivities($scope, $http);
+        });
+  	};
+  	//------------------
+
+  	var editDialogOptions = {
+	    controller: 'EditActivityCtrl',
+	    templateUrl: './activityEdit.html',
+	};
+  	$scope.edit = function(activity){
+   	 	var activityToEdit = activity;
+    	$dialog.dialog(angular.extend(editDialogOptions, {resolve: {activity: angular.copy(activityToEdit)}})).open().then(function (){
+    	    loadActivities($scope, $http);
+        }) ;
+  	};
+
+	$scope.delete = function(activity) {
+		var deleteRequest = {
+			method : 'DELETE',
+			url: 'activity/' + activity.id
+		};
+
+  		$http(deleteRequest).then(function() {
+			loadActivities($scope, $http);
+		});
+  		//todo handle error
+	};
+});
+app.controller('AddActivityCtrl', function($scope, $http, dialog){
+
+  	$scope.save = function(Activity) {
+  		var postRequest = {
+    	method : 'POST',
+       	url: 'activity' ,
+       	data: {
+  				text: $scope.activity.text,
+  				tags: $scope.activity.tags,
+  				title: $scope.activity.title
+			  }
+		}
+
+  		$http(postRequest).then(function (response) {
+  		    $scope.activities = response.data;
+  		}).then(function () {
+  			$scope.close();
+  		});
+  	};
+
+  	$scope.close = function(){;
+    	dialog.close(undefined);
+  	};
+});
+app.controller('EditActivityCtrl', function ($scope, $http, activity, dialog) {
+
+	$scope.activity = activity;
+  	$scope.save = function($activity) {
+  	    var putRequest = {
+    	method : 'PUT',
+       	url: 'activity/' + $scope.activity.id,
+       	data: {
+  				text: $scope.activity.text,
+  				tags: $scope.activity.tags,
+  				title: $scope.activity.title
+			  }
+		}
+
+  		$http(putRequest).then(function (response) {
+  		    $scope.activities = response.data;
+  		}).then(function () {
+			//todo handle error
+			$scope.close();
+		});
+  	};
+
+  	$scope.close = function(){
+  		loadActivities($scope, $http);
+    	dialog.close();
+  	};
+});
